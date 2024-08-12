19093e2d435cea945b22ec0a6cb626a7e258633d
Inna Manush <manush.inna@gmail.com>
2024-08-12 22:38:42 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+// first task
+//
+// console.log(typeof 42);             // number
+// console.log(typeof "Hello");        // string
+// console.log(typeof true);           // boolean
+// console.log(typeof undefined);      // undefined
+// console.log(typeof null);           // object (this is a known JavaScript quirk)
+// console.log(typeof {});             // object
+// console.log(typeof []);             // object (arrays are objects in JavaScript)
+// console.log(typeof function() {});  // function
+//
+// second task
+//
+// let string1 = prompt("Enter the first string");
+// let string2 = prompt("Enter the second string");
+// let string3 = prompt("Enter the third string");
+//
+// third task
+//
+// console.log(`You entered: ${string1}, ${string2}, ${string3}`);
+// let fiveDigitNum = prompt("Enter a five-digit number:");
+// let fiveDigitNumToString = fiveDigitNum.toString();
+// let decomposedFiveDigitNumToString = `${fiveDigitNumToString[0]} ${fiveDigitNumToString[1]} ${fiveDigitNumToString[2]} ${fiveDigitNumToString[3]} ${fiveDigitNumToString[4]}`;
+//
+// console.log(decomposedFiveDigitNumToString);
